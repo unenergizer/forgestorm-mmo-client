fa203ce7eb5678691733b5f4f9d880c2253dcdab
unenergizer <unenergizer@gmail.com>
2018-11-05 23:49:58 -0600

    Map fixes. Tileset fixes.

--- android/assets/maps/tileset/dungeon1.1/Dungeon_B.tsx
+++ android/assets/maps/tileset/dungeon1.1/Dungeon_B.tsx
@@ -1,4 +1,62 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="[Dungeon] Dungeon_B" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="../../textures/dungeon1.1/dungeon_B.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="Dungeon Light Brick" tile="33"/>
+  <terrain name="Dungeon Dark Brick" tile="81"/>
+  <terrain name="Dungeon Brick Red" tile="129"/>
+  <terrain name="Dungeon Brick Gold" tile="177"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,0"/>
+ <tile id="16" terrain=",0,,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,,0,"/>
+ <tile id="19" terrain="0,,0,0"/>
+ <tile id="20" terrain=",0,0,0"/>
+ <tile id="32" terrain=",0,,"/>
+ <tile id="33" terrain="0,0,,"/>
+ <tile id="34" terrain="0,,,"/>
+ <tile id="48" terrain=",,,1"/>
+ <tile id="49" terrain=",,1,1"/>
+ <tile id="50" terrain=",,1,"/>
+ <tile id="51" terrain="1,1,1,"/>
+ <tile id="52" terrain="1,1,,1"/>
+ <tile id="64" terrain=",1,,1"/>
+ <tile id="65" terrain="1,1,1,1"/>
+ <tile id="66" terrain="1,,1,"/>
+ <tile id="67" terrain="1,,1,1"/>
+ <tile id="68" terrain=",1,1,1"/>
+ <tile id="80" terrain=",1,,"/>
+ <tile id="81" terrain="1,1,,"/>
+ <tile id="82" terrain="1,,,"/>
+ <tile id="96" terrain=",,,2"/>
+ <tile id="97" terrain=",,2,2"/>
+ <tile id="98" terrain=",,2,"/>
+ <tile id="99" terrain="2,2,2,"/>
+ <tile id="100" terrain="2,2,,2"/>
+ <tile id="112" terrain=",2,,2"/>
+ <tile id="113" terrain="2,2,2,2"/>
+ <tile id="114" terrain="2,,2,"/>
+ <tile id="115" terrain="2,,2,2"/>
+ <tile id="116" terrain=",2,2,2"/>
+ <tile id="128" terrain=",2,,"/>
+ <tile id="129" terrain="2,2,,"/>
+ <tile id="130" terrain="2,,,"/>
+ <tile id="144" terrain=",,,3"/>
+ <tile id="145" terrain=",,3,3"/>
+ <tile id="146" terrain=",,3,"/>
+ <tile id="147" terrain="3,3,3,"/>
+ <tile id="148" terrain="3,3,,3"/>
+ <tile id="160" terrain=",3,,3"/>
+ <tile id="161" terrain="3,3,3,3"/>
+ <tile id="162" terrain="3,,3,"/>
+ <tile id="163" terrain="3,,3,3"/>
+ <tile id="164" terrain=",3,3,3"/>
+ <tile id="176" terrain=",3,,"/>
+ <tile id="177" terrain="3,3,,"/>
+ <tile id="178" terrain="3,,,"/>
 </tileset>
