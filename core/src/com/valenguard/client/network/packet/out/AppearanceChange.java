70b4abf14af28831e1444cebeb568258227a0fa6
unenergizer <unenergizer@gmail.com>
2018-11-17 01:25:49 -0600

    Allows the client to send the server appearance changes. #2

---
+++ core/src/com/valenguard/client/network/packet/out/AppearanceChange.java
@@ -0,0 +1,23 @@
+package com.valenguard.client.network.packet.out;
+
+import com.valenguard.client.network.shared.Opcodes;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class AppearanceChange extends ClientOutPacket {
+
+    private final short headId, bodyId;
+
+    public AppearanceChange(short headId, short bodyId) {
+        super(Opcodes.APPEARANCE);
+        this.headId = headId;
+        this.bodyId = bodyId;
+    }
+
+    @Override
+    protected void createPacket(ObjectOutputStream write) throws IOException {
+        write.writeShort(headId);
+        write.writeShort(bodyId);
+    }
+}
