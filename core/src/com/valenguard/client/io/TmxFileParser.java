2c00ac1611918637c7cd785b8525ce4ea714fbce
unenergizer <unenergizer@gmail.com>
2019-04-24 23:24:52 -0500

    Cleaned up map parser for future features.

--- core/src/com/valenguard/client/io/TmxFileParser.java
+++ core/src/com/valenguard/client/io/TmxFileParser.java
@@ -20,7 +20,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import static com.valenguard.client.util.Log.println;
 
-@SuppressWarnings({"ConstantConditions", "SpellCheckingInspection"})
 public class TmxFileParser {
 
     private static final boolean PRINT_DEBUG = false;
@@ -32,44 +31,59 @@ public class TmxFileParser {
      * @return A map data class with information about this map.
      */
     public static GameMap loadXMLFile(FileHandle fileHandle) {
-        println(TmxFileParser.class, "Tmx Parsing: " + fileHandle.file().getAbsolutePath(), true, PRINT_DEBUG);
+        Document document = null;
 
         // Lets get the document
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-        DocumentBuilder builder = null;
         try {
-            builder = factory.newDocumentBuilder();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse(fileHandle.read()); // LibGDX fileHandle.read() for Android/PC
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
-        }
-
-        Document document = null;
-        try {
-            document = builder.parse(fileHandle.read());
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (document.getDocumentElement() == null)
-            println(TmxFileParser.class, "Document is null?? 2");
-
         // Get the first element
         Element tmx = document.getDocumentElement();
 
-        /* *********************************************************************************************
-         * BUILD TILED MAP TILES......
-         ***********************************************************************************************/
-
         final short mapWidth = Short.parseShort(tmx.getAttributes().getNamedItem("width").getNodeValue());
         final short mapHeight = Short.parseShort(tmx.getAttributes().getNamedItem("height").getNodeValue());
+        Tile[][] map = parseMapTiles(mapWidth, mapHeight, tmx);
 
-        println(TmxFileParser.class, "MapWidth: " + mapWidth, false, PRINT_DEBUG);
-        println(TmxFileParser.class, "MapHeight: " + mapHeight, false, PRINT_DEBUG);
+        // Examine XML file and find tags called "layer" then loop through them.
+        // NOTE: Element names are CASE sensitive!
+        NodeList objectGroupTag = tmx.getElementsByTagName("objectgroup");
+        for (int i = 0; i < objectGroupTag.getLength(); i++) {
 
-        Tile map[][] = new Tile[mapWidth][mapHeight];
+            // Get Bank Access
+            if (((Element) objectGroupTag.item(i)).getAttribute("name").equals("bank_access")) {
+                NodeList objectTag = ((Element) objectGroupTag.item(i)).getElementsByTagName("object");
+                processBankAccess(mapHeight, map, objectTag);
+            }
+
+            // Get Skill Nodes
+            if (((Element) objectGroupTag.item(i)).getAttribute("name").equals("skill")) {
+                NodeList objectTag = ((Element) objectGroupTag.item(i)).getElementsByTagName("object");
+                processSkillNodes(mapHeight, map, objectTag);
+            }
+
+            // Get Warps
+            if (((Element) objectGroupTag.item(i)).getAttribute("name").equals("warp")) {
+                NodeList objectTag = ((Element) objectGroupTag.item(i)).getElementsByTagName("object");
+                processWarps(mapHeight, map, objectTag);
+            }
+        }
+
+        printMap(mapWidth, mapHeight, map);
+
+        return new GameMap(fileHandle.file().getName().replace(".tmx", ""), mapWidth, mapHeight, map);
+    }
+
+    private static Tile[][] parseMapTiles(short mapWidth, short mapHeight, Element tmx) {
+        Tile[][] map = new Tile[mapWidth][mapHeight];
 
         // Examine XML file and find tags called "layer" then loop through them.
         NodeList layerTag = tmx.getElementsByTagName("layer");
@@ -97,21 +111,15 @@ public class TmxFileParser {
                     // Tile ID
                     int tileType = Integer.parseInt(tile);
 
-                    // Basic coordinate system of our tiled map.
-                    // Iteration starts at top left.
-                    // So if map height is 50, we start fromY at 50
-                    // and then count down (moving down the y access).
+                    // Basic coordinate system of our tiled map. Iteration starts at top left. So if map
+                    // height is 50, we start fromY at 50 and then count down (moving down the y access).
                     // Since X starts on the left, we will start it at 0.
                     //
                     // [X,Y]
                     //
-                    //////////////////////////////////////
-                    // [0,5] [1,5] [2,5] [3,5] [4,5] [5,5]
-                    // [0,4] [1,4] [2,4] [3,4] [4,4] [5,4]
-                    // [0,3] [1,3] [2,3] [3,3] [4,3] [5,3]
-                    // [0,2] [1,2] [2,2] [3,2] [4,2] [5,2]
-                    // [0,1] [1,1] [2,1] [3,1] [4,1] [5,1]
-                    // [0,0] [1,0] [2,0] [3,0] [4,0] [5,0]
+                    // [0,2] [1,2] [2,2]
+                    // [0,1] [1,1] [2,1]
+                    // [0,0] [1,0] [2,0]
 
                     // Initializing the instance of the new tile
                     map[currentX][currentY] = new Tile();
@@ -135,105 +143,83 @@ public class TmxFileParser {
                 }
             }
         }
+        return map;
+    }
 
-        /* *********************************************************************************************
-         * GET SPECIFIC TILE ATTRIBUTES - WARNING: Element names are CASE sensitive!
-         ***********************************************************************************************/
+    private static void processBankAccess(short mapHeight, Tile[][] map, NodeList objectTag) {
+        for (int j = 0; j < objectTag.getLength(); j++) {
+            if (objectTag.item(j).getNodeType() != Node.ELEMENT_NODE) continue;
 
-        // Examine XML file and find tags called "layer" then loop through them.
-        NodeList objectGroupTag = tmx.getElementsByTagName("objectgroup");
+            Element objectTagElement = (Element) objectTag.item(j);
 
-        for (int i = 0; i < objectGroupTag.getLength(); i++) {
+            short x = (short) (Short.parseShort(objectTagElement.getAttribute("x")) / ClientConstants.TILE_SIZE);
+            short y = (short) (mapHeight - (Short.parseShort(objectTagElement.getAttribute("y")) / ClientConstants.TILE_SIZE) - 1);
 
-            /*
-             * Get WARPS
-             */
-            if (((Element) objectGroupTag.item(i)).getAttribute("name").equals("warp")) {
+            println(TmxFileParser.class, "X = " + x, false, PRINT_DEBUG);
+            println(TmxFileParser.class, "Y = " + y, false, PRINT_DEBUG);
 
-                NodeList objectTag = ((Element) objectGroupTag.item(i)).getElementsByTagName("object");
+            // Making it's associated tile non-traversable
+            map[x][y].addFlag(Tile.BANK_ACCESS);
 
-                for (int j = 0; j < objectTag.getLength(); j++) {
-                    if (objectTag.item(j).getNodeType() != Node.ELEMENT_NODE) continue;
+        }
+    }
 
-                    Element objectTagElement = (Element) objectTag.item(j);
-                    short tmxFileX = (short) (Short.parseShort(objectTagElement.getAttribute("x")) / ClientConstants.TILE_SIZE);
-                    short tmxFileY = (short) (Short.parseShort(objectTagElement.getAttribute("y")) / ClientConstants.TILE_SIZE);
-                    short tmxFileWidth = (short) (Short.parseShort(objectTagElement.getAttribute("width")) / ClientConstants.TILE_SIZE);
-                    short tmxFileHeight = (short) (Short.parseShort(objectTagElement.getAttribute("height")) / ClientConstants.TILE_SIZE);
+    private static void processSkillNodes(short mapHeight, Tile[][] map, NodeList objectTag) {
+        for (int j = 0; j < objectTag.getLength(); j++) {
 
-                    for (int ii = tmxFileY; ii < tmxFileY + tmxFileHeight; ii++) {
-                        for (int jj = tmxFileX; jj < tmxFileX + tmxFileWidth; jj++) {
-                            Tile tile = map[jj][mapHeight - ii - 1];
-                            tile.addFlag(Tile.WARP);
-                            tile.setCursorDrawType(CursorDrawType.WARP);
-                        }
-                    }
-                }
-            }
+            if (objectTag.item(j).getNodeType() != Node.ELEMENT_NODE) continue;
 
-            /*
-             *  Get BANK ACCESS
-             */
-            if (((Element) objectGroupTag.item(i)).getAttribute("name").equals("bank_access")) {
+            Element objectTagElement = (Element) objectTag.item(j);
 
-                NodeList objectTag = ((Element) objectGroupTag.item(i)).getElementsByTagName("object");
+            short x = (short) (Short.parseShort(objectTagElement.getAttribute("x")) / ClientConstants.TILE_SIZE);
+            short y = (short) (mapHeight - (Short.parseShort(objectTagElement.getAttribute("y")) / ClientConstants.TILE_SIZE) - 1);
 
-                for (int j = 0; j < objectTag.getLength(); j++) {
-                    if (objectTag.item(j).getNodeType() != Node.ELEMENT_NODE) continue;
+            // Making it's associated tile non-traversable
+            map[x][y].removeFlag(Tile.TRAVERSABLE);
+            map[x][y].setCursorDrawType(CursorDrawType.MINING);
+        }
+    }
 
-                    Element objectTagElement = (Element) objectTag.item(j);
+    private static void processWarps(short mapHeight, Tile[][] map, NodeList objectTag) {
+        for (int j = 0; j < objectTag.getLength(); j++) {
+            if (objectTag.item(j).getNodeType() != Node.ELEMENT_NODE) continue;
 
-                    short x = (short) (Short.parseShort(objectTagElement.getAttribute("x")) / ClientConstants.TILE_SIZE);
-                    short y = (short) (mapHeight - (Short.parseShort(objectTagElement.getAttribute("y")) / ClientConstants.TILE_SIZE) - 1);
+            Element objectTagElement = (Element) objectTag.item(j);
+            short tmxFileX = (short) (Short.parseShort(objectTagElement.getAttribute("x")) / ClientConstants.TILE_SIZE);
+            short tmxFileY = (short) (Short.parseShort(objectTagElement.getAttribute("y")) / ClientConstants.TILE_SIZE);
+            short tmxFileWidth = (short) (Short.parseShort(objectTagElement.getAttribute("width")) / ClientConstants.TILE_SIZE);
+            short tmxFileHeight = (short) (Short.parseShort(objectTagElement.getAttribute("height")) / ClientConstants.TILE_SIZE);
 
-                    println(TmxFileParser.class, "X = " + x, false, PRINT_DEBUG);
-                    println(TmxFileParser.class, "Y = " + y, false, PRINT_DEBUG);
-
-                    // Making it's associated tile non-traversable
-                    map[x][y].addFlag(Tile.BANK_ACCESS);
-
-                }
-            }
-
-            /*
-             * Get SKILL NODES
-             */
-            if (((Element) objectGroupTag.item(i)).getAttribute("name").equals("skill")) {
-                NodeList objectTag = ((Element) objectGroupTag.item(i)).getElementsByTagName("object");
-                for (int j = 0; j < objectTag.getLength(); j++) {
-
-                    if (objectTag.item(j).getNodeType() != Node.ELEMENT_NODE) continue;
-
-                    Element objectTagElement = (Element) objectTag.item(j);
-
-                    short x = (short) (Short.parseShort(objectTagElement.getAttribute("x")) / ClientConstants.TILE_SIZE);
-                    short y = (short) (mapHeight - (Short.parseShort(objectTagElement.getAttribute("y")) / ClientConstants.TILE_SIZE) - 1);
-
-                    // Making it's associated tile non-traversable
-                    map[x][y].removeFlag(Tile.TRAVERSABLE);
-                    map[x][y].setCursorDrawType(CursorDrawType.MINING);
+            for (int ii = tmxFileY; ii < tmxFileY + tmxFileHeight; ii++) {
+                for (int jj = tmxFileX; jj < tmxFileX + tmxFileWidth; jj++) {
+                    Tile tile = map[jj][mapHeight - ii - 1];
+                    tile.addFlag(Tile.WARP);
+                    tile.setCursorDrawType(CursorDrawType.WARP);
                 }
             }
         }
+    }
 
-        /*
-         * Print the map to console.
-         */
-        if (PRINT_DEBUG) {
-            short yOffset = (short) (mapHeight - 1);
-            for (short height = yOffset; height >= 0; height--) {
-                for (short width = 0; width < mapWidth; width++) {
-                    Tile tile = map[width][height];
-                    if (!tile.isFlagSet(Tile.TRAVERSABLE)) System.out.print("X");
-                    else if (tile.isFlagSet(Tile.TRAVERSABLE) && tile.isFlagSet(Tile.WARP))
-                        System.out.print("@");
-                    else if (tile.isFlagSet(Tile.TRAVERSABLE) && !tile.isFlagSet(Tile.WARP))
-                        System.out.print(" ");
-                }
-                System.out.println();
+    /**
+     * Print the map to console.
+     */
+    @SuppressWarnings("UnusedAssignment")
+    private static void printMap(short mapWidth, short mapHeight, Tile[][] map) {
+        if (!PRINT_DEBUG) return;
+        println(TmxFileParser.class, "MapWidth: " + mapWidth, false);
+        println(TmxFileParser.class, "MapHeight: " + mapHeight, false);
+        short yOffset = (short) (mapHeight - 1);
+        for (short height = yOffset; height >= 0; height--) {
+            for (short width = 0; width < mapWidth; width++) {
+                Tile tile = map[width][height];
+                if (!tile.isFlagSet(Tile.TRAVERSABLE)) System.out.print("X");
+                else if (tile.isFlagSet(Tile.TRAVERSABLE) && tile.isFlagSet(Tile.WARP))
+                    System.out.print("@");
+                else if (tile.isFlagSet(Tile.TRAVERSABLE) && !tile.isFlagSet(Tile.WARP))
+                    System.out.print(" ");
             }
-            System.out.println(); // Clear a line for next map
+            println(true);
         }
-        return new GameMap(fileHandle.file().getName().replace(".tmx", ""), mapWidth, mapHeight, map);
+        println(true); // Clear a line for next map
     }
 }
