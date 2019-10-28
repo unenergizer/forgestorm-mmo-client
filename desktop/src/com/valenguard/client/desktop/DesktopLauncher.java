7d21583c3b633f6a31c99f8f28db80ac6d8474a5
unenergizer <unenergizer@gmail.com>
2019-10-27 19:27:19 -0500

    Changed command line arguments.

--- desktop/src/com/valenguard/client/desktop/DesktopLauncher.java
+++ desktop/src/com/valenguard/client/desktop/DesktopLauncher.java
@@ -6,12 +6,23 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.valenguard.client.ClientConstants;
 import com.valenguard.client.Valenguard;
 
+import static com.valenguard.client.util.Log.println;
+
 public class DesktopLauncher {
+
+    private static final String unArg = "username:";
+    private static final String pwArg = "password:";
+
     public static void main(String[] args) {
 
         boolean ideRun = false;
+        String username = null;
+        String password = null;
+
         for (int i = 0; i < args.length; i++) {
-            if (i == 0) if (args[0].equalsIgnoreCase("ideRun")) ideRun = true;
+            if (args[i].equalsIgnoreCase("ideRun")) ideRun = true;
+            if (args[i].contains(unArg)) username = args[i].replace(unArg, "");
+            if (args[i].contains(pwArg)) password = args[i].replace(pwArg, "");
         }
 
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -28,9 +39,13 @@ public class DesktopLauncher {
         Valenguard valenguard = Valenguard.getInstance();
         valenguard.setIdeRun(ideRun);
 
-        if (ideRun) {
-            valenguard.getLoginCredentials().setUsername("ForgeStorm");
-            valenguard.getLoginCredentials().setPassword("abc123");
+        if (username != null && !username.isEmpty()) {
+            println(DesktopLauncher.class, "Username: " + username);
+            valenguard.getLoginCredentials().setUsername(username);
+        }
+        if (password != null && !password.isEmpty()) {
+            println(DesktopLauncher.class, "Password: " + password);
+            valenguard.getLoginCredentials().setPassword(password);
         }
 
         new LwjglApplication(valenguard, config);
