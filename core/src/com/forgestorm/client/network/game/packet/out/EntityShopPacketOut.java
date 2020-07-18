674673d8616ac0eff4908a4de9377e2bb8521ba1
Joseph <bintvec@gmail.com>
2020-07-18 12:51:02 -0400

    Example of a packet having a reference removed.

--- core/src/com/forgestorm/client/network/game/packet/out/EntityShopPacketOut.java
+++ core/src/com/forgestorm/client/network/game/packet/out/EntityShopPacketOut.java
@@ -1,27 +1,32 @@
 package com.forgestorm.client.network.game.packet.out;
 
 import com.forgestorm.client.game.rpg.EntityShopAction;
+import com.forgestorm.client.game.rpg.ShopOpcodes;
 import com.forgestorm.client.network.game.shared.Opcodes;
 
 public class EntityShopPacketOut extends AbstractClientPacketOut {
 
-    private EntityShopAction entityShopAction;
+    private ShopOpcodes shopOpcode;
+    private short shopSlot;
+    private short entityId;
 
     public EntityShopPacketOut(EntityShopAction entityShopAction) {
         super(Opcodes.ENTITY_SHOPS);
-        this.entityShopAction = entityShopAction;
+        shopOpcode = entityShopAction.getShopOpcode();
+        shopSlot = entityShopAction.getShopSlot();
+        entityId = entityShopAction.getEntityId();
     }
 
     @Override
     void createPacket(ForgeStormOutputStream write) {
-        write.writeByte(entityShopAction.getShopOpcode().getShopOpcodeByte());
+        write.writeByte(shopOpcode.getShopOpcodeByte());
 
-        switch (entityShopAction.getShopOpcode()) {
+        switch (shopOpcode) {
             case BUY:
-                write.writeShort(entityShopAction.getShopSlot());
+                write.writeShort(shopSlot);
                 break;
             case START_SHOPPING:
-                write.writeShort(entityShopAction.getEntityId());
+                write.writeShort(entityId);
                 break;
         }
     }
