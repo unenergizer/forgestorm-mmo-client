491b153d5b43ec21959d0f03b03f736ba594872a
unenergizer <unenergizer@gmail.com>
2023-10-08 21:17:36 -0500

    Fixed comparison contract with compareTo method. Fix by TEttinger.

--- core/src/main/java/com/forgestorm/client/game/world/WorldObject.java
+++ core/src/main/java/com/forgestorm/client/game/world/WorldObject.java
@@ -15,12 +15,9 @@ public class WorldObject implements Comparable<WorldObject> {
 
     @Override
     public int compareTo(WorldObject otherWorldObject) {
-        // Solve drawY and drawX sorting (fast)
-//        return (int)(Math.signum(otherWorldObject.getDrawY() - this.getDrawY()) * 2 + Math.signum(otherWorldObject.getDrawX() - this.getDrawX()));
-
         // Solve drawY and drawX sorting (faster - @TEttinger#4280 - libGDX discord)
-        int x = Float.floatToIntBits(otherWorldObject.getDrawX() - this.getDrawX());
-        int y = Float.floatToIntBits(otherWorldObject.getDrawY() - this.getDrawY());
+        int x = Float.floatToIntBits(otherWorldObject.getDrawX() - this.getDrawX() + 0f);
+        int y = Float.floatToIntBits(otherWorldObject.getDrawY() - this.getDrawY() + 0f);
         return ((y >> 31 | -y >>> 31) << 1) + (x >> 31 | -x >>> 31);
     }
 
