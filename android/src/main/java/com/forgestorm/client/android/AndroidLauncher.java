f0fdfc601ab8dc15f328e9d3465624559818c2a7
unenergizer <unenergizer@gmail.com>
2023-10-11 18:59:40 -0500

    Get Android build partially working.

--- android/src/main/java/com/forgestorm/client/android/AndroidLauncher.java
+++ android/src/main/java/com/forgestorm/client/android/AndroidLauncher.java
@@ -1,7 +1,9 @@
 package com.forgestorm.client.android;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.forgestorm.client.ClientMain;
@@ -13,6 +15,15 @@ public class AndroidLauncher extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+        System.out.println("using this ohe");
+
+//        ClientMain clientMain = ClientMain.getInstance();
         initialize(new ClientMain(), configuration);
     }
 }
